bdd58d0f4c31475c76f4bda4994571921e307b83
DimityrStoyanov <dimityrstoyanov@gmail.com>
2023-09-21 18:14:03 +0300

    Prepare for new exam
    New exam day.

---
+++ JS 09.23 -Programming Fundamentals - SoftUni/20.09 - Data Types and Variables/01. Data Types.js
@@ -0,0 +1,8 @@
+let a = 1;
+console.log( typeof a)
+
+let b = `Mitko`
+console.log( typeof b)
+
+let c = undefined
+console.log( typeof c)
---
+++ JS 09.23 -Programming Fundamentals - SoftUni/20.09 - Data Types and Variables/03. Right Place.js
@@ -0,0 +1,5 @@
+function dataTypes(word, symbol, match){
+
+
+}
+dataTypes(`Str_ng`, `I`, `Strong`);
