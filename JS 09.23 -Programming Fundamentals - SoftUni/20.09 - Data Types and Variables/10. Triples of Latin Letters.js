18df0a9930fbaf5fa9564db74e651d71a5792ac7
DimityrStoyanov <dimityrstoyanov@gmail.com>
2023-09-22 16:22:45 +0300

    Final Work for Exercises

--- JS 09.23 -Programming Fundamentals - SoftUni/20.09 - Data Types and Variables/10. Triples of Latin Letters.js
+++ JS 09.23 -Programming Fundamentals - SoftUni/20.09 - Data Types and Variables/10. Triples of Latin Letters.js
@@ -1,5 +1,14 @@
-function latin(){
+function latin(num) {
+    let sum = 0;
+    for (let a = 1; a <= num; a++) {
 
+        for (let b = 1; b <= num; b++){
+
+            for( let c = 1; c <= num; c++){
+                console.log(`${String.fromCharCode(96+a)}${String.fromCharCode(96+b)}${String.fromCharCode(96+c)}`)
+            }
+        }
+    }
 
 }
-latin()
+latin(3)
