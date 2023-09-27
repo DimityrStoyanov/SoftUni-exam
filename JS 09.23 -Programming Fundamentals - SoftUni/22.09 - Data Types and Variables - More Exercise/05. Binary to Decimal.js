f1186326aed1719ed4454cc54017c2a9cadadad4
DimityrStoyanov <dimityrstoyanov@gmail.com>
2023-09-27 09:43:13 +0300

    Still working on it

--- JS 09.23 -Programming Fundamentals - SoftUni/22.09 - Data Types and Variables - More Exercise/05. Binary to Decimal.js
+++ JS 09.23 -Programming Fundamentals - SoftUni/22.09 - Data Types and Variables - More Exercise/05. Binary to Decimal.js
@@ -1,7 +1,8 @@
 function binary(num){
 
-
+    let digit = parseInt(num, 2);
+    console.log(digit);
 }
-binary(00001001)
-binary(11110000)
-binary(00100100)    
+binary(`00001001`)
+binary(`11110000`)
+binary(`00100100`)    
---
+++ JS 09.23 -Programming Fundamentals - SoftUni/22.09 - Data Types and Variables - More Exercise/06. Chess Board.js
@@ -0,0 +1,24 @@
+function chessBoard(){
+    <div class="chessboard">
+ <div>
+ <span class="black"></span>
+ <span class="white"></span>
+ <span class="black"></span>
+ </div>
+ <div>
+ <span class="white"></span>
+ <span class="black"></span>
+ <span class="white"></span>
+ </div>
+ <div>
+ <span class="black"></span>
+ <span class="white"></span>
+ <span class="black"></span>
+ </div>
+</div>
+
+
+}
+chessBoard(3)
+chessBoard(4)
+chessBoard(5)
