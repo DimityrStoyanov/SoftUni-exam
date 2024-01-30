f5a9b1d06243d685eb93f5917993f8c439144f42
DimityrStoyanov <dimityrstoyanov@gmail.com>
2024-01-30 17:53:10 +0200

    DOM Manipulation

--- JS 01.24 - Advanced/01.24.24 DOM Manipulation/01. List-Of-Items/app.js
+++ JS 01.24 - Advanced/01.24.24 DOM Manipulation/01. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    console.log('TODO:..');
+    let items = document.getElementById("items");
+    let input = document.getElementById("newItemText").value;
+    let inputValue = input
+    let liElement = document.createElement(`li`)
+    items.appendChild(liElement).innerText = inputValue
+    document.getElementById("newItemText").value = ``
+
 }
--- JS 01.24 - Advanced/01.24.24 DOM Manipulation/02. Delete-From-Table/app.js
+++ JS 01.24 - Advanced/01.24.24 DOM Manipulation/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    console.log('TODO:..');
+    let input = document.querySelector(`input[name="email"]`);
+    let result = document.querySelector(`#result`)
+    let rows = document.querySelectorAll(`#customers tbody tr`);
+
+    for (let row of rows) {
+        let colums = row.children[1];
+        if (colums.innerText == input.value) {
+            row.remove();
+            input.value = ``
+            result.innerText = `Deleted.`
+        }  else {
+            result.innerText = "Not found." 
+        }
+    }
+
 }
